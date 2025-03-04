cfc60cac0ee5a99b26881d4f0cf3daf7454ad360
Spike Mak <msh.spike@gmail.com>
2025-03-04 11:53:58 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # CO2-Sensor
+
+## Description
+
+This is a simple Arduino sketch to control a CCS811 air quality sensor with LCD1602 display and a relay to turn on and off a fan when the CO2 level exceeds a threshold.
+
+## Required Libraries
+
+- `Adafruit_CCS811` - Library for CCS811 air quality sensor
+- `Wire` - For I2C communication with the sensor (usually included with Arduino IDE)
+- `LiquidCrystal_I2C` - For LCD1602 with I2C backpack
+  
+## Wiring
+
+### Pin Connections
+
+| Component | Arduino Pin |
+|-----------|------------|
+| CCS811 SDA | A4 (SDA) |
+| CCS811 SCL | A5 (SCL) |
+| CCS811 VCC | 3.3V |
+| CCS811 GND | GND |
+| LCD1602 SDA | A4 (SDA) |
+| LCD1602 SCL | A5 (SCL) |
+| LCD1602 VCC | 5V |
+| LCD1602 GND | GND |
+| Relay Signal | D4 |
+| Fan +12V | Relay NO (Normally Open) |
+| Fan GND | Power Supply GND |
+
+### Fritzing Diagram
+
+![Wiring Diagram](./images/wiring_diagram.png)
+// Note: You'll need to create and add the actual wiring diagram image
+
+### Note
+Both the CCS811 and LCD1602 use I2C communication. Make sure they have different I2C addresses to avoid conflicts. The default address for:
+- LCD1602 with I2C backpack is typically 0x27 or 0x3F
+- CCS811 is typically 0x5A or 0x5B
---
+++ air_quality_control.ino
@@ -0,0 +1,74 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_CCS811.h>
+
+// Initialize objects
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Set the LCD address to 0x27 for a 16 chars and 2 line display
+Adafruit_CCS811 ccs;
+
+// Define pins
+const int RELAY_PIN = 4;  // Connect relay to digital pin 4
+const int CO2_THRESHOLD = 1000;  // CO2 threshold in ppm to trigger fan
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Initialize LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.print("Initializing...");
+  
+  // Initialize relay pin
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);  // Start with fan off
+  
+  // Initialize CCS811
+  if(!ccs.begin()){
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    lcd.clear();
+    lcd.print("Sensor Error!");
+    while(1);
+  }
+  
+  // Wait for the sensor to be ready
+  while(!ccs.available());
+  
+  lcd.clear();
+}
+
+void loop() {
+  if(ccs.available()){
+    if(!ccs.readData()){
+      int co2 = ccs.geteCO2();
+      int tvoc = ccs.getTVOC();
+      
+      // Update LCD display
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("CO2: ");
+      lcd.print(co2);
+      lcd.print(" ppm");
+      
+      lcd.setCursor(0,1);
+      lcd.print("TVOC: ");
+      lcd.print(tvoc);
+      lcd.print(" ppb");
+      
+      // Control fan based on CO2 levels
+      if(co2 > CO2_THRESHOLD){
+        digitalWrite(RELAY_PIN, HIGH);  // Turn fan on
+      } else {
+        digitalWrite(RELAY_PIN, LOW);   // Turn fan off
+      }
+      
+      // Print to Serial for debugging
+      Serial.print("CO2: ");
+      Serial.print(co2);
+      Serial.print("ppm, TVOC: ");
+      Serial.print(tvoc);
+      Serial.println("ppb");
+    }
+  }
+  delay(1000);  // Wait for a second before next reading
+} 
